486a19a16212373b7f5fada5c7747d26e0f3deb1
João <jvdotto@gmail.com>
2023-06-02 14:03:27 -0300

    feat: add a new script for fuzzy in matlab

--- T02/Fuzzy_csv_import.m
+++ T02/Fuzzy_csv_import.m
@@ -4,5 +4,5 @@ opts = detectImportOptions('treino_sinais_vitais_com_label.txt');
 preview('treino_sinais_vitais_com_label.txt',opts)
 opts.SelectedVariableNames = [4 5 6];
 fuzzy_inputs = readmatrix('treino_sinais_vitais_com_label.txt', opts);
-opts.SelectedVariableNames = [8];
+opts.SelectedVariableNames = [7];
 fuzzy_outputs = readmatrix('treino_sinais_vitais_com_label.txt', opts);
