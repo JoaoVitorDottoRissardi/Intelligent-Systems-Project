5f078ce809eea34cddc1d6807c1b44ee640fb79c
Jhonnykva <51447639+Jhonnykva@users.noreply.github.com>
2023-06-29 20:29:45 -0300

    Arquivos Fuzzy

--- T02/FuzzyMatlab/Fuzzy_csv_import.m
+++
@@ -1,8 +0,0 @@
-clear ; close all; clc
-
-opts = detectImportOptions('../treino_sinais_vitais_com_label.txt');
-preview('../treino_sinais_vitais_com_label.txt',opts)
-opts.SelectedVariableNames = [4 5 6];
-fuzzy_inputs = readmatrix('../treino_sinais_vitais_com_label.txt', opts);
-opts.SelectedVariableNames = [7];
-fuzzy_outputs = readmatrix('../treino_sinais_vitais_com_label.txt', opts);
